4f5a509ec4a50f5c2ecdf67141dbeb699e3857c2
Elijah <elijah@Elijahs-MacBook-Air.local>
2024-01-02 17:21:48 +0800

    fix tabs to 4

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,8 +1,8 @@
 vim.cmd("set expandtab")
 vim.cmd("set number")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
 vim.cmd("filetype plugin indent on")
 vim.g.mapleader = " "
 vim.api.nvim_set_hl(0, 'LineNr', { fg='yellow' })
